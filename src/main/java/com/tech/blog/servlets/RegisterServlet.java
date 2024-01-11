81328b700cea1271a846e451018010d9635c25d5
Sakshi Mandal <sakshimandal3456@gmail.com>
2024-01-11 18:16:47 +0530

    Tech Blog Project

---
+++ src/main/java/com/tech/blog/servlets/RegisterServlet.java
@@ -0,0 +1,62 @@
+package com.tech.blog.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @param out 
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response, PrintWriter out) throws ServletException, IOException {
+		//fetch all form data
+		String check=request.getParameter("check");
+		if(check==null)
+		{
+		out.println("box not checked");
+		}else {
+			String name=request.getParameter("user_name");
+			String email=request.getParameter("user_email");
+			String password=request.getParameter("user_password");
+			String gender=request.getParameter("user_gender");
+			String about=request.getParameter("about");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
